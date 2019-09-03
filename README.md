216f0ed6f3178c9e19d58ae583b0b4c39a1e6f13
Jay R Bolton <jayrbolton@gmail.com>
2019-09-03 16:43:33 -0700

    Merge in code and methods from kbase_workspace_utils

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # KBase Workspace - Python Client
 
-Generic workspace client for [KBase's workspace service](https://kbase.us/services/ws/docs).
+Generic, pip-installable workspace client for [KBase's workspace service](https://kbase.us/services/ws/docs).
+
+This package covers many common tasks when working with the KBase Workspace.
+
+Note that you cannot write any data to the workspace using this module. You'll have to use the KBase SDK for that.
 
 ## Installation
 
@@ -8,7 +12,7 @@ Install with pip:
 
 ```sh
 pip install --extra-index-url https://pypi.anaconda.org/kbase/simple \
-    kbase-workspace-client==0.0.2
+    kbase-workspace-client==0.2.0
 ```
 
 ## Usage
@@ -19,15 +23,16 @@ Import and initialize the `WorkspaceClient` class:
 from kbase_workspace_client import WorkspaceClient
 
 ws_client = WorkspaceClient(
-  url="https://appdev.kbase.us/services/ws",
+  url="https://appdev.kbase.us/services/",
   token="my_authentication_token"
 )
 ```
 
-`url` should be the url of KBase services, which will be one of:
-* `https://ci.kbase.us/services/ws`
-* `https://appdev.kbase.us/services/ws`
-* `https://kbase.us/services/ws`
+`url` should be the base URL for the KBase services, which will be one of:
+
+* `https://ci.kbase.us/services/`
+* `https://appdev.kbase.us/services/`
+* `https://kbase.us/services/`
 
 `token` should be a developer or service authentication token.
 
@@ -85,9 +90,54 @@ You can stream the workspace response to a file by using:
 
 The `file_path` must be a non-existent file in a writable directory.
 
-### Exceptions
+## Misc. utilities
 
-#### WorkspaceResponseError
+### ws_client.download_shock_file(shock_id, dest_path)
+
+Download a file given a shock ID and a destination path:
+
+```py
+ws_client.download_shock_file('unique_shock_id', dest_path)
+```
+
+`dest_path` must point to a non-existent file in a writable directory.
+
+This download will be streaming and low-memory.
+
+### ws_client.handle_to_shock(handle_id)
+
+Get the shock ID from a handle ID in order to download the file:
+
+```py
+shock_id = ws_client.handle_to_shock(handle_id)
+ws_client.download_shock_file(shock_id, dest_path)
+```
+
+### ws_client.download_assembly_fasta(ref, save_dir, admin=False)
+
+Download the FASTA for an Assembly (or legacy ContigSet) datatype to a directory.
+
+Options:
+* `admin` - whether or not to download as an admin or as a normal user
+
+```py
+ws_client.download_assembly_fasta("1/2/3", "/tmp/xyz")
+```
+
+### ws_client.download_reads_fastq(ref, save_dir, admin=False)
+
+Download the fastq for a PairedEndLibrary or SingleEndLibrary datatype to a directory.
+
+Options:
+* `admin` - whether or not to download as an admin or as a normal user
+
+```py
+ws_client.download_reads_fastq("1/2/3", "/tmp/xyz")
+```
+
+## Exceptions
+
+### WorkspaceResponseError
 
 Raised on any invalid response from the workspace. Properties on the error object are:
 
@@ -113,29 +163,6 @@ except WorkspaceResponseError as err:
 ```
 
 `WorkspaceResponseError` is a child of `RuntimeError`.
-
-## Downloading attached files
-
-### ws_client.download_from_shock(shock_id, dest_path)
-
-Download a file given a shock ID and a destination path:
-
-```py
-ws_client.download_from_shock('unique_shock_id', dest_path)
-```
-
-`dest_path` must point to a non-existent file in a writable directory.
-
-This download will be streaming and low-memory.
-
-### ws_client.handle_to_shock(handle_id)
-
-Get the shock ID from a handle ID in order to download the file:
-
-```py
-shock_id = ws_client.handle_to_shock(handle_id)
-ws_client.download_from_shock(shock_id, dest_path)
-```
 
 ## Development
 
--- src/test/test_main.py
+++ src/test/test_main.py
@@ -1,11 +1,14 @@
 import os
 import unittest
+import tempfile
+import shutil
 from src.kbase_workspace_client import WorkspaceClient, WorkspaceResponseError
+from src.kbase_workspace_client.exceptions import InvalidWSType, InvalidGenome
 
 if not os.environ.get('TEST_TOKEN'):
     raise RuntimeError("TEST_TOKEN environment variable is required.")
 
-_URL = "https://ci.kbase.us/services/ws"
+_URL = "https://ci.kbase.us/services/"
 _ws_client = WorkspaceClient(url=_URL, token=os.environ['TEST_TOKEN'])
 
 
@@ -72,3 +75,79 @@ class TestMain(unittest.TestCase):
                 'objects': [{'ref': _id}],
                 'no_data': 1
             })
+
+    def test_assembly_download_valid(self):
+        try:
+            tmp_dir = tempfile.mkdtemp()
+            valid_ws_id = '34819/10/1'
+            pathname = _ws_client.download_assembly_fasta(valid_ws_id, tmp_dir)
+            self.assertEqual(os.path.getsize(pathname), 3849120)
+            filename = os.path.basename(pathname)
+            self.assertEqual(filename, "MEGAHIT.contigs.fasta")
+        finally:
+            shutil.rmtree(tmp_dir)
+
+    # Error cases for invalid users and invalid ws references are covered in test_download_obj
+
+    def test_assembly_download_wrong_type(self):
+        try:
+            reads_id = '15/45/1'
+            tmp_dir = tempfile.mkdtemp()
+            with self.assertRaises(InvalidWSType) as err:
+                _ws_client.download_assembly_fasta(ref=reads_id, save_dir=tmp_dir)
+            self.assertTrue('Invalid workspace type' in str(err.exception))
+        finally:
+            shutil.rmtree(tmp_dir)
+
+    def test_reads_download_valid(self):
+        """
+        Test valid downloads for both paired and single-end reads. Paired-end has examples for both
+        interleaved and not.
+        """
+        tmp_dir = tempfile.mkdtemp()
+        # Paired reads, non-interleaved
+        ref = '15/45/1'
+        paths = _ws_client.download_reads_fastq(ref=ref, save_dir=tmp_dir)
+        self.assertEqual(len(paths), 2)
+        self.assertTrue('rhodobacter.art.q10.PE.reads.paired.fwd.fastq' in paths[0])
+        self.assertTrue('rhodobacter.art.q10.PE.reads.paired.rev.fastq' in paths[1])
+        self.assertEqual(os.path.getsize(paths[0]), 36056522)
+        self.assertEqual(os.path.getsize(paths[1]), 37522557)
+        # Paired reads, interleaved
+        ref = '15/44/1'
+        paths = _ws_client.download_reads_fastq(ref=ref, save_dir=tmp_dir)
+        self.assertTrue('rhodobacter.art.q20.int.PE.reads.paired.interleaved.fastq' in paths[0])
+        self.assertEqual(len(paths), 1)
+        self.assertEqual(os.path.getsize(paths[0]), 36510129)
+        # Single-end reads
+        ref = '15/43/1'
+        paths = _ws_client.download_reads_fastq(ref=ref, save_dir=tmp_dir)
+        self.assertTrue('rhodobacter.art.q50.SE.reads.single.fastq' in paths[0])
+        self.assertEqual(len(paths), 1)
+        self.assertEqual(os.path.getsize(paths[0]), 53949468)
+        shutil.rmtree(tmp_dir)
+
+    # Error cases for invalid users and invalid ws references are covered in test_download_obj
+
+    def test_reads_download_wrong_type(self):
+        assembly_id = '34819/10/1'
+        tmp_dir = tempfile.mkdtemp()
+        with self.assertRaises(InvalidWSType) as err:
+            _ws_client.download_reads_fastq(ref=assembly_id, save_dir=tmp_dir)
+        self.assertTrue('Invalid workspace type' in str(err.exception))
+        shutil.rmtree(tmp_dir)
+
+    def test_get_assembly_from_genome(self):
+        """Test the valid/successful case."""
+        ref = '34819/14/1'
+        assembly_ref = _ws_client.get_assembly_from_genome(ref)
+        self.assertEqual(assembly_ref, '34819/14/1;16/7/1')
+
+    def test_get_assembly_from_genome_missing_assembly(self):
+        """
+        Test the case where a Genome object does not have an assembly_ref
+        """
+        ref = '34819/5/9'
+        with self.assertRaises(InvalidGenome) as err:
+            _ws_client.get_assembly_from_genome(ref)
+        self.assertTrue('no assembly or contigset references' in str(err.exception))
