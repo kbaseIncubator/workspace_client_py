57ad9d1ff5bd964262da039fdfd17a7db792b69a
Jay R Bolton <jayrbolton@gmail.com>
2020-08-06 11:54:07 -0700

    Add example code to fetch all narrative upas

---
+++ examples/get_all_narrative_refs.py
@@ -0,0 +1,23 @@
+"""
+Print all narrative UPAs for an environment to stdout
+"""
+import os
+
+from kbase_workspace_client import WorkspaceClient
+
+assert 'TOKEN' in os.environ, 'env var for a KBase auth token is required (TOKEN)'
+assert 'KBASE_ENDPOINT' in os.environ, 'env var for KBase endpoint is required (KBASE_ENDPOINT)'
+
+ws_client = WorkspaceClient(
+  url=os.environ['KBASE_ENDPOINT'],
+  token=os.environ["TOKEN"],
+)
+
+narr_ids = []
+for wsid in range(1, 60000):
+    try:
+        for objinfo in ws_client.generate_obj_infos(wsid, admin=True):
+            if objinfo[2] == 'KBaseNarrative.Narrative-4.0':
+                print(f"{objinfo[6]}/{objinfo[0]}/{objinfo[4]}")
+    except Exception:
+        continue
